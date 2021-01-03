9fc7315a13726eaa4a825549155d91e3a7254207
John Salas <johndsalas@gmail.com>
2021-01-03 09:48:13 -0600

    Set up for project complete

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+This program takes in an unsolved Sudoku board and returns the solved version of that board.
+'''
+
+board = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+        ]
+
+print(board)
