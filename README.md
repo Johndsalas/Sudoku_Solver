4eff857f73684c9a6525af80668a87c3768d3333
John Salas <johndsalas@gmail.com>
2021-01-07 19:04:35 -0600

    Finished Solver

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Sudoku_Solver
 
-This repository will be used to create an algorithm for solving Sudoku puzzles using backtracking.
+This repository contains an algorithm that takes in an unsolved sudoko puzzle as a list of lists and prints the unsolved and solved vertions of that board. The sudoku is solved using a backtracking algorithm.
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ This program takes in an unsolved Sudoku board and returns the solved version of
 '''
 
 # save unsolved board as list of lists
-sudoku = [
+board = [
             [0,0,2,0,0,0,4,0,0],
             [0,0,0,4,0,2,8,0,0],
             [0,9,0,0,7,0,0,0,2],
@@ -15,15 +15,14 @@ sudoku = [
             [0,0,8,0,0,0,6,0,0],
         ]
 
-# print unsolved board
-
-def print_board(board):
+# print board
+def print_board(bo):
     '''
-    Prints unsolved Sedoku board from list of list of numbers
+    Prints Sedoku board from list of list of numbers
     '''
     
     # itterate through each row number
-    for i in range(len(board)):
+    for i in range(len(bo)):
 
         # for each row number if it is divisable by three and is not zero print dividing line
         if (i % 3 == 0) and (i != 0):
@@ -32,7 +31,7 @@ def print_board(board):
             print("- - - - - - - - - -")
 
         # itterate through each column number
-        for j in range(len(board[0])):
+        for j in range(len(bo[0])):
 
             # for each row number if it is divisable by three and is not zero
             if (j % 3 == 0) and (j != 0):
@@ -44,64 +43,112 @@ def print_board(board):
             if j == 8:
 
                 # print the number located at borard[row_number][column_number]
-                print(board[i][j])
+                print(bo[i][j])
 
             # other wise print the number located at borard[row_number][column_number] stay on the current line
             else:
 
-                print(str(board[i][j]) + ' ', end = "")
+                print(str(bo[i][j]) + ' ', end = "")
 
-def get_empty(board):
+def find_empty(bo):
     '''
     Get next empty space on the board
     '''
 
     # itterate through each row number and each column number
-    for i in range(len(sudoku)):
-        for j in range(len(sudoku[0])):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
 
             # if the position of the row and column has a value of 0 (an empty space on the board)
             # return a tuple with the row and column number
-            if board[i][j] == 0:
+            if bo[i][j] == 0:
                 return (i,j) # (row,column)
 
-def valid(board, number, pos):
+    return False
+
+def valid(bo, num, pos):
     '''
     Verifies if a given number is a valid entry for a given position on the current sudoku board
     '''
 
     # check row
-    # examines a (row, column) pair.
-    # itterates through each row in the named column
-    # checks if the value in each row matches the value in the named row
-    # returns False if any of the values match
-    for j in range(len(board[0])):
+    # itterates through each other position in the row
+    # returns false if there are any matches
+    for i in range(len(bo[0])):
 
-        if board[j][pos[0]] == number and pos[0] != j:
+        if bo[pos[0]][i] == num and pos[1] != i:
 
             return False
-
+    
     # check column
-    # examines a (row, column) pair.
-    # itterates through each column in the named row
-    # checks if the value in each column matches the value in the named column
-    # returns False if any of the values match
-    for i in range(len(board)):
+    # itterates through each other position in the column
+    # returns false if there are any matches
+    for i in range(len(bo)):
 
-        if board[pos[0]][i] == number and pos[1] != i:
+        if bo[i][pos[1]] == num and pos[0] != i:
 
             return False
 
     # check box
-    # gets x and y coordinate for box a (row, column) position is in 
+    # gets x and y coordinate for box a (row, column) position is in
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-    # itterates through values in the box and returns false if and of the values match a specified value 
+    # itterates through values in the box and returns false if and of the values match a specified value
     for i in range(box_y * 3, (box_y * 3) + 3):
 
         for j in range(box_x * 3, (box_x * 3) + 3):
 
-            if board[i][j] == number and (i,j) != pos:
+            if board[i][j] == num and (i,j) != pos:
 
                 return False
+
+    return True
+
+def solve(bo):
+    '''
+    Solve a sudoku board
+    '''
+
+    # get position of the first empty place on the board
+    # or return True if there are no empty places remaining
+    find = find_empty(bo)
+    if not find:
+
+        return True
+
+    else:
+
+        row, col = find
+
+    # itterate through all possible values for position (1-9)
+    for i in range(1,10):
+
+        # if a value can be legally placed at the given position do so
+        # otherwise move to the next value
+        if valid(bo, i, (row, col)):
+
+            bo[row][col] = i
+
+            # if calling solve again results no correct values
+            # set the current position to a value to zero
+            # continue itterating through i values untill a new legal value is found
+            # put that value in the current position
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+def make_it_happen(bo):
+    '''
+    Prints out unsolved and solved vertion of sudoku board
+    '''
+    print_board(bo)
+    solve(bo)
+    print('')
+    print('')
+    print_board(bo)
+
+make_it_happen(board)
